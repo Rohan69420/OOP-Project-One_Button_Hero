b7c02bfa75db8bf14fc42c111b794b073b98175c
RohanM <rohan13maharjan@gmail.com>
2022-07-27 22:02:01 +0545

    button class added

---
+++ ButtonHandler.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SDL.h>
+enum LButtonSprite
+{
+	BUTTON_SPRITE_MOUSE_OUT = 0,
+	BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
+	BUTTON_SPRITE_MOUSE_DOWN = 2,
+	BUTTON_SPRITE_MOUSE_UP = 3,
+	BUTTON_SPRITE_TOTAL = 4
+};
+class LButton
+{
+public:
+	//Initializes internal variables
+	LButton();
+
+	//Sets top left position
+	void setPosition(int x, int y);
+
+	//Handles mouse event
+	void handleEvent(SDL_Event* e);
+
+	//Shows button sprite
+	void render();
+
+private:
+	//Top left position
+	SDL_Point mPosition; //<<<<<<<<<<<<<<<<<<<<<<<<This new 
+
+	//Currently used global sprite
+	LButtonSprite mCurrentSprite;
+};
