d7667fea88256ed4fec9825789e160b183c43b31
RohanM <rohan13maharjan@gmail.com>
2022-08-15 12:09:07 +0545

    New class for player movement and collisions

---
+++ PlayerHandler.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Game.h"
+#include "GlobalDetails.h"
+class Player{
+public:
+	//The dimensions of the dot
+	static const int DOT_WIDTH = SPRITEW;
+	static const int DOT_HEIGHT = SPRITEH;
+
+	//Maximum axis velocity of the dot
+	static const int DOT_VEL = 10;
+
+	//Initializes the variables
+	Player();
+
+	//Takes key presses and adjusts the dot's velocity
+	void handleEvent(SDL_Event& e);					///<<<<<< updated event handler
+
+	//Moves the dot
+	void move();	///<<<<<<<<<	-	-	-	-	-	-	-		-
+
+	//Shows the dot on the screen
+	void render(SDL_Rect *);
+
+
+private:
+	//The X and Y offsets of the dot
+	int mPosX, mPosY;					//<<<	<	<	<	<	< -	-	-	-	-
+
+	//The velocity of the dot
+	int mVelX, mVelY;
+};
